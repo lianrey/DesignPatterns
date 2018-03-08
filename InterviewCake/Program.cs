ce50111d7930c9877c639fd2ecfda3cf73f88e8a
lianrey <lianareyesx@gmail.com>
2018-03-07 18:13:05 -0600

    Interview cake - question 1

---
+++ InterviewCake/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InterviewCake
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // QUESTION 1
+            var stockPrices = new int[] { 12, 10, 7, 5, 11, 9, 2 };
+            Console.WriteLine(Question1.GetMaxProfit(stockPrices));
+
+            // QUESTION 2
+        }
+    }
+}
---
+++ InterviewCake/Question1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterviewCake
+{
+    class Question1
+    {
+        public static int GetMaxProfit(int[] stockPricesYesterday)
+        {
+            if (stockPricesYesterday.Length < 2)
+            {
+                throw new ArgumentException("Getting a profit requires at least 2 prices", nameof(stockPricesYesterday));
+            }
+
+            // Calculate the max profit
+            int minPrice = stockPricesYesterday[0];
+            int max = stockPricesYesterday[1] - stockPricesYesterday[0];
+
+            for (int i = 1; i < stockPricesYesterday.Length; i++)
+            {
+
+                int currentPrice = stockPricesYesterday[i];
+
+                int potentialProfit = currentPrice - minPrice;
+
+                max = Math.Max(max, potentialProfit);
+
+                minPrice = Math.Min(minPrice, currentPrice);
+            }
+
+            return max;
+        }
+    }
+}
