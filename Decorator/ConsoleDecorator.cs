b2a161aa17cf1d184a2014426f761ad74aacdb48
lianrey <lianareyesx@gmail.com>
2018-03-18 15:40:32 -0500

    Add decorator example

---
+++ Decorator/ConsoleDecorator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Decorator
+{
+    public static class ConsoleDecorator
+    {
+        public static void Write(string text, int size){
+            string newLine = "";
+            for (int i = 0; i < size; i++)
+            {
+                newLine += "\n";
+            }
+            Console.Write(newLine + text);
+        }
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Decorator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // http://www.newthinktank.com/2012/09/decorator-design-pattern-tutorial/
+            ConsoleDecorator.Write("Lalala", 10);
+        }
+    }
+}
