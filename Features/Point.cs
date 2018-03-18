ee7fa4ad29452c7ff370b092fee1f15ecd6a57f7
lianrey <lianareyesx@gmail.com>
2018-03-18 16:32:05 -0500

    Overwrite operators + - ==

---
+++ Features/Point.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Features
+{
+    public class Point
+    {
+        public int Height
+        {
+            get;
+            set;
+        }
+
+        public Point(int height){
+            Height = height;
+        }
+
+        public static Point operator+ (Point a, Point b){
+            Point point = new Point(a.Height + b.Height);
+            return point;
+        }
+
+        public static Point operator- (Point a, Point b)
+        {
+            Point point = new Point(a.Height - b.Height);
+            return point;
+        }
+
+        public static bool operator ==(Point a, Point b)
+        {
+            return (a.Height == b.Height);
+        }
+
+        public static bool operator !=(Point a, Point b)
+        {
+            return (a.Height != b.Height);
+        }
+
+    }
+}
---
+++ Features/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Features
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Point point1 = new Point(10);
+            Point point2 = new Point(15);
+            Point point3 = new Point(10);
+
+            var result_sum = point1 + point2;
+            Console.WriteLine(result_sum.Height);
+
+            var result_substraction = point3 - point2;
+            Console.WriteLine(result_substraction.Height);
+
+            if (point1 == point3) Console.WriteLine("Are equals");
+        }
+    }
+}
