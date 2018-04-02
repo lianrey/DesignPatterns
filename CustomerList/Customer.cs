be511598b9fe490658aa220696d20d05ddf15c4e
lianrey <lianareyesx@gmail.com>
2018-04-01 23:02:00 -0500

    read from files ex

---
+++ CustomerList/Customer.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+namespace CustomerList
+{
+    public class Customer
+    {
+        public string Name;
+
+        public string City;
+
+        public int Balance;
+
+        public Guid code;
+
+        public Customer(string name, string city){
+            Name = name;
+            City = city;
+            Random r = new Random();
+            Balance = r.Next(0, 100000);
+            code = new Guid();
+        }
+    }
+}
---
+++ CustomerList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CustomerList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string namesFile = File.ReadAllText(@"Files/names.txt");
+            String[] names = namesFile.Split("\n");
+
+            string citiesFile = File.ReadAllText(@"Files/cities.txt");
+            String[] cities = citiesFile.Split("\n");
+
+            List<Customer> customers = new List<Customer>();
+            foreach (var item in names)
+            {
+                Random r = new Random();
+                customers.Add(new Customer(item, cities[r.Next(0, cities.Length-1)]));
+
+            }
+            foreach (var c in customers)
+            {
+                Console.WriteLine($"Customer: {c.Name}, city: {c.City}, random: {c.Balance}");
+            }
+        }
+    }
+}
